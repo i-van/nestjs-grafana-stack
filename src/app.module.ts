22552046866ee21fd6756678aceaa62e4a14c814
Ivan Nosov <i.k.nosov@gmail.com>
2023-10-02 15:13:42 -0700

    fix: change histogram help message

--- src/app.module.ts
+++ src/app.module.ts
@@ -25,7 +25,7 @@ import { LoggingInterceptor } from './logging.interceptor';
     AppService,
     makeHistogramProvider({
       name: 'http_request_duration_seconds',
-      help: 'Duration of HTTP requests in microseconds',
+      help: 'Duration of HTTP requests in seconds',
       labelNames: ['method', 'route', 'code'],
       buckets: [0.1, 0.3, 0.5, 0.7, 1, 3, 5, 7, 10],
     }),
